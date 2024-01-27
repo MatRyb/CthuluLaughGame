984540a7d646dfd865c43dff1a05d49f6766a7f4
adanadhar <abukkeinib@gmail.com>
2024-01-27 02:19:26 +0100

    Enemy mov+att

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,20 +1,52 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private Transform target;
-    [SerializeField] private float NavMeshAgentspeed;
-    
+    [SerializeField] private float Speed = 2;
+    private float enemyCooldown = 1;
+    private float damage = 1;
+    private bool playerInRange = false;
+    private bool canAttack = true;
 
     private void Update()
     {
         if (target != null)
         {
             agent.SetDestination(target.position);
-            NavMeshAgent.speed = NavMeshAgentspeed;
+            agent.speed = Speed;
+        }
+
+        if (playerInRange && canAttack)
+        {
+            //GameObject.Find("Player").GetComponent<ControllerForPlayer>().currentHealth -= damage;
+            StartCoroutine(AttackCooldown());
+            Debug.Log("Attack");
         }
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) ;
+        {
+            playerInRange = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) ;
+        {
+            playerInRange = false;
+        }
+    }
+    IEnumerator AttackCooldown()
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(enemyCooldown);
+        canAttack = true;
+    }
 }
